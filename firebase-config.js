00c45156010f184c6dcf37bee47d851e9c26aeb4
yedam2357 <202430914@cheoncheon.hs.kr>
2024-07-03 10:09:50 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+
+// Firebase 프로젝트 구성 정보
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+
+// Firebase 데이터베이스 참조
+var database = firebase.database();
