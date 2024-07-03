1c87af7d46e5d56a6c76081782ab10b1de899d56
Starplayer39 <72770312+Starplayer39@users.noreply.github.com>
2024-07-03 22:15:51 +0900

    Update submit-form.js

--- submit-form.js
+++ submit-form.js
@@ -167,4 +167,7 @@ document.getElementById('grading-form').addEventListener('submit', function(even
       document.getElementById('submit-result').innerHTML = '<p>채점 중 오류가 발생했습니다. 다시 시도해주세요.</p>';
     }
   }
+
+  gradeExam();
+  
 });
