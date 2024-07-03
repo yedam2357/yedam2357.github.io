9bcd6811cd3a1b32158fb451a8cc28ace74d9d1c
yedam2357 <202430914@cheoncheon.hs.kr>
2024-07-03 11:42:06 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,23 @@
 // firebase-config.js
 
-// Firebase 프로젝트 구성 정보
-var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase 초기화
-firebase.initializeApp(firebaseConfig);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBUr426b4ZjegpcJ6-gvtirjI-TdsrZ7Y",
+    authDomain: "test-67eeb.firebaseapp.com",
+    projectId: "test-67eeb",
+    storageBucket: "test-67eeb.appspot.com",
+    messagingSenderId: "157964586912",
+    appId: "1:157964586912:web:7b2926cd3bb1b152811ec5",
+    measurementId: "G-TFF1H0SFXE"
+  };
 
-// Firebase 데이터베이스 참조
-var database = firebase.database();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
